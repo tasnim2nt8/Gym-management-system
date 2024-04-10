6f6df9e7748f68fa6ddc7a2ce4a8a25492b0b54a
Nishat Tasnim <tasnim.2nt8@gmail.com>
2024-04-10 16:36:16 +0600

    Remove blank line

--- fitnesshub/fitnesshub/settings.py
+++ fitnesshub/fitnesshub/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 from django.contrib.messages import constants as messages
 
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
